4ab7aefb4938bf644491f1411e4133de26050b0a
gtiburcio <guilherme.tiburcio001@gmail.com>
2020-12-27 17:54:25 -0300

    first commit

---
+++ src/main/java/br/com/tiburcio/springlearn/controller/LearnController.java
@@ -0,0 +1,13 @@
+package br.com.tiburcio.springlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LearnController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
